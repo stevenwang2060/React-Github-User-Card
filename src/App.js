23ae953def25c812dab2d9aaa8bd2088d5bfac5e
Steven Wang <stevenwang2060@gmail.com>
2020-03-10 15:20:28 -0700

    Created axios call, user profile and followers populating. Styling added with styled components. Github calendar added. Reached MVP and stretch.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import UserCard from "./components/UserCard";
+
+import GitHubCalendar from 'react-github-calendar';
+
+class App extends Component {
+  
+  state = {
+    myUser : [],
+    followers: [],
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users/stevenwang2060')
+      .then(response => {
+          console.log(`user data from CDM`, response);
+          this.setState({
+            myUser : response.data
+          })
+      })
+        .catch(error => {
+            console.log("The data was not returned", error);
+      })
+  
+    axios
+      .get('https://api.github.com/users/stevenwang2060/followers')
+      .then(response => {
+          console.log(`followers initial`, response.data);
+          this.setState({ 
+            followers : response.data
+          });
+      })
+        .catch(error => {
+          console.log("The data was not returned", error);
+      });
+  }
+
+
+  render() {
+      return (
+      <div className="App">
+        <h2>GitHub User Cards</h2>
+        <GitHubCalendar username='stevenwang2060' />
+        <UserCard 
+          myUser = {this.state.myUser}  
+          followers = {this.state.followers}
+        />
+        
+      </div>
+    );
+  };
 }
 
 export default App;
